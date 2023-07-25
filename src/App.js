647d70938da120969d2db31c324271a3ebe12a84
rupali patil <rupalishewale2010@gmail.com>
2023-07-25 17:30:18 +0530

    working on it

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import './App.css';
 import TaskOne from "./TaskOne/Main"
 import TaskThree from "./TaskThree/Main"
 import TaskFour from "./TaskFour/Main"
-import TaskFive from "./TaskFive/Main"
 import TaskTwo from './TaskTwo/Main'
-import FiveTask from "./FiveTask/Main"
 function App() {
   return (
     <>
      <TaskOne/>
      {/* <TaskThree/> */}
      {/* <TaskFour/> */}
-     {/* <TaskFive/> */}
      {/* <TaskTwo/> */}
-     {/* <FiveTask/> */}
     </>
   );
 }
